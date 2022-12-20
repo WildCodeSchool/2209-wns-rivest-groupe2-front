b730e5444ea5d2580f43c0931c0e33a09227ae5a
charlinedev <charlinebarbelette@yahoo.fr>
2022-12-20 23:42:37 +0100

    update map

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,10 @@
-
-// import image from "../Home/img/paris-map.png";
+import image from ".\asset\img\map.png";
 
 const Home = () => {
   return (
     <>
     <div className="w-full flex items-center">
-    {/* <div className="bg-cover bg-center" style={{ backgroundImage: `url(${image})` }}> */}
+    <div className="bg-cover bg-center" style={{ backgroundImage: `url(${image})` }}>
     <div className="box-border flex items-center h-32 w-32 p-4 border-4 shadow-2xl">
         <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
             <option>Paris</option>
@@ -14,7 +13,7 @@ const Home = () => {
             <option>Marseille</option>
         </select>
     </div>
-    {/* </div> */}
+    </div>
     </div>
     </>
 );
