ad01df65aab225e63d3c326b0b32ab90613e0c02
Yan Labarthe <yan.labarthe@gmail.com>
2022-12-22 09:41:44 +0100

    Main deleted and App hydrated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Dashboard } from './layouts/dashboard';
-import { Auth } from './layouts/auth';
 import Home from './pages/Home/Home';
-import Main from './Main';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import NotFound from './pages/NotFound/NotFound';
+import Payment from './pages/Payment/Payment';
+import POICreation from './pages/POICreation/POICreation';
+import POIDetails from './pages/POIDetails/POIDetails';
+import Profil from './pages/Profil/Profil';
+import TownCreation from './pages/TownCreation/TownCreation';
+import Dashboard from './layouts/dashboard';
+import Auth from './layouts/auth';
+import BaseLayout from './layouts/baseLayout';
+import DashboardHome from './pages/dashboard/home';
 
-function App() {
-  return <Main />;
-}
+const App = () => (
+  <>
+    <Routes>
+      <Route path="/" element={<BaseLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/point-of-interest/creation" element={<POICreation />} />
+        <Route path="/point-of-interest/:id" element={<POIDetails />} />
+        <Route path="/profil/:id" element={<Profil />} />
+        <Route path="/town/creation" element={<TownCreation />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+      <Route path="/" element={<DashboardHome />} />
+      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+    </Routes>
+  </>
+);
 
 export default App;
