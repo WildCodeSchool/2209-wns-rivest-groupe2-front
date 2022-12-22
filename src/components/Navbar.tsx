4c3bfe8d369c298b870bf50ab4230d498c6bac05
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-12-22 16:06:25 +0100

    poilist page + map marker

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,22 +9,41 @@ const getActiveLinkStyle = ({ isActive }: { isActive: boolean }) => ({
 const Navbar = () => {
   return (
     <nav>
-      <ul>
+      <ul className="flex justify-between items-center m-5">
         <li>
           <NavLink to="/" style={getActiveLinkStyle}>
             <img src={icon} alt="icon site" />
           </NavLink>
         </li>
-        <li>
-          <NavLink to="/signin" style={getActiveLinkStyle}>
-            <p>SignIn</p>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/signup" style={getActiveLinkStyle}>
-            <p>SignUp</p>
-          </NavLink>
-        </li>
+        <select
+          name="cities"
+          id="cities"
+          className="bg-white p-[4px] pl-[15px] mt-2 border border-2 rounded-xl w-[300px]"
+        >
+          <option value="City" disabled>
+            City
+          </option>
+          <option value="Paris" selected>
+            Paris
+          </option>
+          <option value="Lyon">Lyon</option>
+          <option value="Marseille">Marseille</option>
+          <option value="Bordeaux">Bordeaux</option>
+          <option value="Bordeaux">Bordeaux</option>
+          <option value="Toulouse">Toulouse</option>
+        </select>
+        <div className="flex">
+          <li className="pr-5">
+            <NavLink to="/signin" style={getActiveLinkStyle}>
+              <p>Sign In</p>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup" style={getActiveLinkStyle}>
+              <p>Sign Up</p>
+            </NavLink>
+          </li>
+        </div>
       </ul>
     </nav>
   );
