49982e149e055c9300c9875218e25a83be193d7c
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-12-22 17:44:27 +0100

    link home to poilist

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,29 +8,16 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.json'],
+    project: ['./front/tsconfig.json'],
   },
   root: true,
   rules: {
-    '@typescript-eslint/no-misused-promises': [
-      'error',
-      {
-        checksVoidReturn: false,
-      },
-    ],
-    'object-curly-spacing': 'error',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/strict-boolean-expressions': 'off',
     '@typescript-eslint/no-floating-promises': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/require-array-sort-compare': [
-      'error',
-      {
-        ignoreStringArrays: true,
-      },
-    ],
-    'eol-last': ['error', 'always'],
-    'no-multiple-empty-lines': 'error',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    'no-unused-vars': 'off',
   },
 };
