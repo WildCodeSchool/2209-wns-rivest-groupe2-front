a04db500620822ffb5fb2cfc1a988b87ea3dadb5
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-12-22 09:45:15 +0100

    create poilist page + init map + create navbar

--- src/components/MapModule.tsx
+++ src/components/MapModule.tsx
@@ -1,5 +1,35 @@
+import { LatLngExpression } from 'leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+
+function MapComponent() {
+  const map = useMap();
+  return null;
+}
+
 const MapModule = () => {
-  return <div></div>;
+  const parisPosition: LatLngExpression | undefined = [48.866667, 2.333333];
+  return (
+    <MapContainer
+      center={parisPosition}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{
+        height: 500,
+        width: '50%',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginBottom: '10px',
+        borderRadius: '10px',
+      }}
+    >
+      <MapComponent />
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {/* <Marker position={[45.764043, 4.835659]} /> */}
+    </MapContainer>
+  );
 };
 
 export default MapModule;
