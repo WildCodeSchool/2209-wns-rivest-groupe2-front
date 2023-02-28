ba1ae168750ab12cf0c4e68509f7c4fe96cbc85b
charlinedev <charlinebarbelette@yahoo.fr>
2023-02-28 17:34:57 +0100

    updatePoiDetail

--- src/pages/POIDetails/POIDetails.tsx
+++ src/pages/POIDetails/POIDetails.tsx
@@ -68,7 +68,7 @@ const thisPOI = data?.getAllPoi?.find((poi: { id: number; }) => poi.id === Numbe
     </nav>
     {/* Image gallery */}   
          {/* <Gallery */}
-              pictureUrl={thisPOI.pictureUrl}        
+              pictures... {thisPOI.pictureUrl}        
         {/* /> */}
   {/* POI Detail */}
   <div className="product-detail-desc">
@@ -112,77 +112,38 @@ const thisPOI = data?.getAllPoi?.find((poi: { id: number; }) => poi.id === Numbe
           <h3 className="sr-only">Reviews</h3>
           <div className="flex items-center">
             <div className="flex items-center">
-              {/* <!--
-                Heroicon name: mini/star
-
-                Active: "text-gray-900", Default: "text-gray-200"
-              --> */}
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+              {/* <!-- Heroicon name: heart filled --> */}
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
               </svg>
-
-              {/* <!-- Heroicon name: mini/star --> */}
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
-              </svg>
-
-              {/* <!-- Heroicon name: mini/star --> */}
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
-              </svg>
-
-              {/* <!-- Heroicon name: mini/star --> */}
-              <svg className="text-gray-900 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
-              </svg>
-
-              {/* <!-- Heroicon name: mini/star --> */}
-              <svg className="text-gray-200 h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z" clip-rule="evenodd" />
+              {/* <!-- Heroicon name: heart empty --> */}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
               </svg>
             </div>
             <p className="sr-only">4 out of 5 stars</p>
-            <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 reviews</a>
+            <a href="#" className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">117 likes</a>
           </div>
         </div>
 
         
       </div>
       
-      {/* <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
-        {/* <!-- Description and details --> 
+       <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pt-6 lg:pb-16 lg:pr-8">
+         {/* <!-- Description and details -->  */}
         <div>
-          <h3 className="sr-only">Description</h3>
-
           <div className="space-y-6">
             <p className="text-base text-gray-900"></p>
           </div>
         </div>
-
-        <div className="mt-10">
-          <h3 className="text-sm font-medium text-gray-900">Highlights</h3>
-
-          <div className="mt-4">
-            <ul role="list" className="list-disc space-y-2 pl-4 text-sm">
-              <li className="text-gray-400"><span className="text-gray-600">----------------</span></li>
-
-              <li className="text-gray-400"><span className="text-gray-600">------------</span></li>
-
-              <li className="text-gray-400"><span className="text-gray-600">-------------</span></li>
-
-              <li className="text-gray-400"><span className="text-gray-600">-------------</span></li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="mt-10">
-          <h2 className="text-sm font-medium text-gray-900">Details</h2>
+        <div className="mt-1">
+          <h2 className="text-sm font-medium text-gray-900">Description:</h2>
 
           <div className="mt-4 space-y-6">
-            <p className="text-sm text-gray-600">blablabla</p>
+            <p className="text-sm text-gray-600">{thisPOI.description}...</p>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   </div>
 </div>
