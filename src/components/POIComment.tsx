271b3b2d5cd8c911edd7a241abc7c5bb0e4f7114
Yan Labarthe <yan.labarthe@gmail.com>
2023-06-21 14:42:03 +0200

    a

--- src/components/POIComment.tsx
+++ src/components/POIComment.tsx
@@ -182,7 +182,7 @@ const POIComment: React.FC<POIDetailsProps> = ({
               onChange={handleCommentChange}
             />
             <button type="submit" className="bg-blue-500 text-white p-2 mt-2">
-              Soumetre
+              Soumettre
             </button>
           </form>
         )}
